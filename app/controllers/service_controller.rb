79fbf574ea0acb482906696a5f3efa87c75e96ae
Aparna Karve <akarve@redhat.com>
2016-03-28 16:08:16 -0700

    Added Angular route for Edit

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -159,7 +159,6 @@ class ServiceController < ApplicationController
       @service = find_by_id_filtered(Service, checked[0])
       @in_a_form = true
       @title = _("Editing %{model} \"%{name}\"") % {:name => @service.name, :model => ui_lookup(:model => "Service")}
-      return
     end
   end
 
@@ -392,6 +391,10 @@ class ServiceController < ApplicationController
 
   def tagging_explorer_controller?
     @explorer
+  end
+
+  def angular_app?
+    true
   end
 
   def get_session_data
